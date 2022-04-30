1464e61d5032ee977d76027e2949056d4f0ac9c9
Elmo Lin <alvin4883889@gmail.com>
2022-04-30 11:07:03 +0800

    binary search

---
+++ 367.Valid-Perfect-Square.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left=1;
+        int right=num;
+        while(left<=right){
+            long mid=left+(right-left)/2;
+            if(mid*mid==num){
+                return true;
+            }
+            else if(mid*mid<num){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return false;
+    }
+};
